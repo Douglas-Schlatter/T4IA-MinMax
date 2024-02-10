be4d40053f0e8833967401a9349859e9c7e0ce32
joaoPauloDias <joaopaulovasquezdias@gmail.com>
2024-02-10 00:49:25 -0300

    add 🐵🙈🐘🙉🙊🦍

---
+++ entrega/minimax.py
@@ -0,0 +1,56 @@
+import random
+from typing import Tuple, Callable
+
+
+
+def minimax_move(state, max_depth:int, eval_func:Callable) -> Tuple[int, int]:
+    """
+    Returns a move computed by the minimax algorithm with alpha-beta pruning for the given game state.
+    :param state: state to make the move (instance of GameState)
+    :param max_depth: maximum depth of search (-1 = unlimited)
+    :param eval_func: the function to evaluate a terminal or leaf state (when search is interrupted at max_depth)
+                    This function should take a GameState object and a string identifying the player,
+                    and should return a float value representing the utility of the state for the player.
+    :return: (int, int) tuple with x, y coordinates of the move (remember: 0 is the first row/column)
+    """
+    alpha = -10**9
+    beta = 10**9
+
+    (value, move) =  maxr(state, alpha, beta, max_depth, 0, eval_func, state.player,None)
+    return move
+    
+def maxr(state,alpha:float, beta:float, max_depth, depth, eval_func, player,lastMove):
+    #print(state.board)
+    if (((depth >= max_depth) and (max_depth != -1)) or state.is_terminal()):
+        return(eval_func(state, player), lastMove)
+    else:
+        bestValue = -10**9
+        bestMove = None
+        for move in state.legal_moves():
+            (value, nextMove) = minr(state.next_state(move), alpha, beta, max_depth, depth+1, eval_func, player, move)
+            if (value > bestValue):
+                bestValue = value
+                bestMove = move
+            alpha = max(value,alpha)
+            if (alpha >= beta):
+                break
+    return (bestValue, bestMove)
+
+def minr(state,alpha:float, beta:float, max_depth, depth, eval_func, player,lastMove):
+    #print(state.board)
+    if (((depth >= max_depth) and (max_depth != -1)) or state.is_terminal()):
+        return(eval_func(state, player), lastMove)
+    else:
+        worstValue = 10**9
+        worstMove = None
+        for move in state.legal_moves():
+            (value, nextMove) = maxr(state.next_state(move), alpha, beta, max_depth, depth+1, eval_func, player, move)
+            if (value < worstValue):
+                worstValue = value
+                worstMove = move
+            
+            beta = min(value,beta)
+            if (alpha >= beta):
+                break
+    return (worstValue, worstMove)
+
---
+++ entrega/othello_minimax_custom.py
@@ -0,0 +1,68 @@
+import random
+from typing import Tuple
+from ..othello.gamestate import GameState
+from ..othello.board import Board
+from .minimax import minimax_move
+
+# Voce pode criar funcoes auxiliares neste arquivo
+# e tambem modulos auxiliares neste pacote.
+#
+# Nao esqueca de renomear 'your_agent' com o nome
+# do seu agente.
+
+EVAL_TEMPLATE = [
+    [100, -30, 6, 2, 2, 6, -30, 100],
+    [-30, -50, 1, 1, 1, 1, -50, -30],
+    [  6,   1, 1, 1, 1, 1,   1,   6],
+    [  2,   1, 1, 3, 3, 1,   1,   2],
+    [  2,   1, 1, 3, 3, 1,   1,   2],
+    [  6,   1, 1, 1, 1, 1,   1,   6],
+    [-30, -50, 1, 1, 1, 1, -50, -30],
+    [100, -30, 6, 2, 2, 6, -30, 100]
+]
+
+
+def make_move(state) -> Tuple[int, int]:
+    """
+    Returns a move for the given game state
+    :param state: state to make the move
+    :return: (int, int) tuple with x, y coordinates of the move (remember: 0 is the first row/column)
+    """
+
+    # o codigo abaixo apenas retorna um movimento aleatorio valido para
+    # a primeira jogada 
+    # Remova-o e coloque uma chamada para o minimax_move (que vc implementara' no modulo minimax).
+    # A chamada a minimax_move deve receber sua funcao evaluate como parametro.
+
+    max_depth = 3  
+    return minimax_move(state, max_depth, evaluate_custom)
+
+
+def evaluate_custom(state, player:str) -> float:
+    """
+    Evaluates an othello state from the point of view of the given player. 
+    If the state is terminal, returns its utility. 
+    If non-terminal, returns an estimate of its value based on your custom heuristic
+    :param state: state to evaluate (instance of GameState)
+    :param player: player to evaluate the state for (B or W)
+    """
+    
+    black = state.board.num_pieces('B')
+    white = state.board.num_pieces('W')
+    if (player == 'B'):
+        vPecas =  black - white
+    else:
+        vPecas =  white - black
+    
+    vPos = 0
+    opponent = 'B' if player == 'W' else 'W'
+    for x in range(8):
+        for y in range(8):
+            if state.board.tiles[x][y] == player:
+                vPos += EVAL_TEMPLATE[x][y]
+            elif state.board.tiles[x][y] == opponent:
+                vPos -= EVAL_TEMPLATE[x][y]
+    
+    return 0.80*vPos + 0.20*vPecas
+
+
